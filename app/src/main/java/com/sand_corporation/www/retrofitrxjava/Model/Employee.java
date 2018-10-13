9c3b117ff1f58d00ee7d1ef03c8d416d3d59f06b
Saifullah Al Mujahid <mujahid7292@gmail.com>
2018-10-13 08:31:19 +0600

    Model Created

---
+++ app/src/main/java/com/sand_corporation/www/retrofitrxjava/Model/Employee.java
@@ -0,0 +1,35 @@
+package com.sand_corporation.www.retrofitrxjava.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Employee {
+
+    @SerializedName("First_Name")
+    @Expose
+    private String firstName;
+
+    @SerializedName("Last_Name")
+    @Expose
+    private String lastName;
+
+    public Employee() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ app/src/main/java/com/sand_corporation/www/retrofitrxjava/Model/EmployeeList.java
@@ -0,0 +1,25 @@
+package com.sand_corporation.www.retrofitrxjava.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class EmployeeList {
+
+    @SerializedName("Employee_List")
+    @Expose
+    private List<Employee> employeeList = null;
+
+    public EmployeeList() {
+
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+}
